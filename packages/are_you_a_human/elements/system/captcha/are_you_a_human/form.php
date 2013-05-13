1da7f51e34df480de40928ca238d250a13a46920
Greg Joyce <greg@concretecms.com>
2013-05-13 16:46:03 -0700

    some documentation hints and links

--- packages/are_you_a_human/elements/system/captcha/are_you_a_human/form.php
+++ packages/are_you_a_human/elements/system/captcha/are_you_a_human/form.php
@@ -4,6 +4,8 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 Loader::model('package');
 $form = Loader::helper('form');
 $pkg = Package::getByHandle('are_you_a_human');
+$ayahLink = '<a href="http://areyouahuman.com/" target="_blank">'.t('Are You A Human').'</a>';
+$docsLink = '<a href="http://www.concrete5.org/marketplace/addons/are_you_a_human/documentation/" target="_blank">'.t('documentation').'</a>';
 ?>
 
 <div class="clearfix">
@@ -20,3 +22,10 @@ $pkg = Package::getByHandle('are_you_a_human');
 		<?= $form->text('ARE_YOU_A_HUMAN_SCORING_KEY', $pkg->config('ARE_YOU_A_HUMAN_SCORING_KEY')) ?>
 	</div>
 </div>
+
+<div class="well">
+	<h4 style="margin-bottom:12px;"><?= t('Not sure what to do here?')?></h4>
+	<p><?= t('You will need an %s account first.',$ayahLink) ?></p>
+	<p><?=t('If you need more information, check out our %s.',$docsLink)?></p>
+</div>
+	

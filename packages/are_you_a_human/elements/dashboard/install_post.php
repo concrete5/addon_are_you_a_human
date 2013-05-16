9d2c64425af35537848bacb15b4966bac78b6a65
Mike Lay <michael.lay.email@gmail.com>
2013-05-16 11:37:51 -0700

    Installation typo

--- packages/are_you_a_human/elements/dashboard/install_post.php
+++ packages/are_you_a_human/elements/dashboard/install_post.php
@@ -8,4 +8,4 @@ $ayahLink = '<a href="http://areyouahuman.com/" target="_blank">'.t('Are You A H
 
 <h2 style="margin-bottom:12px"><?= t('Are You Human') ?></h2>
 <p><?= t('You installed the new captcha system. Go to the system %s to complete setup.',$captchaLink) ?></p>
-<p><?= t('Check out our %s first if you want to learn more aobut how to get started with the %s captcha system.',$docsLink,$ayahLink) ?></p>
+<p><?= t('Check out our %s first if you want to learn more about how to get started with the %s captcha system.',$docsLink,$ayahLink) ?></p>
